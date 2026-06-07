843ce8384401fdc56e6676a2a13eee7386f2c82d
Ferenc <66013194+BFerenc90@users.noreply.github.com>
2026-06-07 07:30:31 +0200

    Update INVENTORY - Users & Groups.ps1

--- Scripts/INVENTORY - Users & Groups.ps1
+++ Scripts/INVENTORY - Users & Groups.ps1
@@ -1,2 +1,34 @@
-Get-LocalUser | select Name, Enabled
-Get-LocalGroup | select Name
+$Groups = Get-CimInstance Win32_Group | Select-Object -ExpandProperty Name
+
+$AdminUsers = Get-WmiObject -Class Win32_GroupUser |
+    Where-Object { $_.GroupComponent -match "Administrators|Rendszergazdák" } |
+    ForEach-Object {
+        ($_.PartComponent -split 'Name="')[1] -split '"' | Select-Object -First 1
+    }
+
+$AllUsers = Get-CimInstance Win32_UserAccount |
+    Select-Object Name, Domain, SID
+
+Write-Host @"
+
+================ USERS AND GROUPS ================
+
+Groups:
+
+$($Groups -join "`n")
+
+Administrators:
+
+$($AdminUsers -join "`n")
+
+Users:
+
+$(
+    $AllUsers | ForEach-Object {
+        "$($_.Domain)\$($_.Name) - SID: $($_.SID)"
+    } | Out-String
+)
+
+===========================================================
+
+"@
