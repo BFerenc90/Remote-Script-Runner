d621269350c839d8254c2248c987d03eb60ae822
Ferenc <66013194+BFerenc90@users.noreply.github.com>
2026-06-07 07:20:59 +0200

    Update INVENTORY - Basic Hardware Info.ps1

--- Scripts/INVENTORY - Basic Hardware Info.ps1
+++ Scripts/INVENTORY - Basic Hardware Info.ps1
@@ -1,13 +1,53 @@
+# Serial Number
+$Bios = Get-CimInstance Win32_BIOS
+$SerialNumber = $Bios.SerialNumber
 
-Get-ComputerInfo | Select `
-    CsName,
-    CsSystemSKUNumber,
-    BiosSerialNumber,
-    @{Name="RAM_GB";Expression={[math]::Round($_.CsTotalPhysicalMemory / 1GB, 2)}},
-    CsProcessors
+# CPU
+$Cpu = Get-CimInstance Win32_Processor
+$CpuName = $Cpu.Name
 
-Get-Volume | Select `
-    DriveLetter,
-    @{Name="FreeSpace_GB";Expression={[math]::Round($_.SizeRemaining / 1GB, 2)}},
-    @{Name="TotalSize_GB";Expression={[math]::Round($_.Size / 1GB, 2)}}
+# RAM
+$TotalRamGB = [math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
 
+# Disk
+$Disks = Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3"
+$DiskInfo = foreach ($Disk in $Disks) {
+    "$($Disk.DeviceID) - Free: $([math]::Round($Disk.FreeSpace / 1GB, 2)) GB / Total: $([math]::Round($Disk.Size / 1GB, 2)) GB"
+}
+
+# BIOS
+$BiosManufacturer = $Bios.Manufacturer
+$BiosVersion = ($Bios.SMBIOSBIOSVersion)
+
+# Motherboard
+$Board = Get-CimInstance Win32_BaseBoard
+$BoardManufacturer = $Board.Manufacturer
+$BoardProduct = $Board.Product
+
+Write-Host @"
+
+================ SYSTEM INFORMATIONS ================
+
+Serial Number:
+$SerialNumber
+
+CPU:
+$CpuName
+
+RAM:
+$TotalRamGB GB
+
+BIOS:
+Manufacturer: $BiosManufacturer
+Version: $BiosVersion
+
+Motherboard:
+Manufacturer: $BoardManufacturer
+Model: $BoardProduct
+
+Disk:
+$($DiskInfo -join "`n")
+
+======================================================
+
+"@
